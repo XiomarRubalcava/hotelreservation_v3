d73b83d5717765d613826b493bba02935edefd04
Xiomar Rubalcava <xiomar.rubalcava@rrgnbs.com>
2025-11-30 01:28:28 -0800

    Fix CORS for production GitHub Pages

--- server.js
+++ server.js
@@ -22,7 +22,8 @@ app.use(cors({
         'http://127.0.0.1:5500',
         'http://localhost:5500',
         'http://localhost:3000',
-        'http://127.0.0.1:3000'
+        'https://xiomarrubalcava.github.io',
+        'https://xiomarrubalcava.github.io/hotelreservation_v3'
     ],
     methods: 'GET,POST,PUT,DELETE',
     credentials: true
